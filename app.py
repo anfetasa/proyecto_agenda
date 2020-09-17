a3fe17c0bba6f9922f544736f14a472d296c4465
Andres Felipe Taborda Sanchez <aftaborda.s@gmail.com>
2020-09-17 13:23:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,9 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session, escape, logging
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
-=======
->>>>>>> 5a26501645a5688d42dfe8e763de57e8321db4d3
-
-@app.route("/eventos")
-def eventos():
-    con = mysql.connection.cursor()
-    con.execute("SELECT * FROM eventos")
-    data = con.fetchall()
-    return render_template("index.html", data=data)
 
 
-@app.route('/agregar', methods=['POST'])
-def agregar():
-    if request.method == 'POST':
-        titulo = request.form['titulo']
-        hora = request.form['hora']
-        fecha = request.form['fecha']
-        descripcion = request.form['descripcion']
-        con = mysql.connection.cursor()
-        con.execute("INSERT INTO eventos (titulo,hora,dia,descripcion) VALUES (%s,%s,%s,%s)",
-                    (titulo, hora, fecha, descripcion,))
-        mysql.connection.commit()
-        return redirect(url_for("index"))
-
-@app.route("/borrar/<string:id>")
-def borrar(id):
-    con = mysql.connection.cursor()
-    con.execute("DELETE FROM eventos WHERE id = {0}".format(id))
-    mysql.connection.commit()
-    return redirect(url_for("index"))
-
-
-@app.route('/cambiar/<id>', methods=['POST', 'GET'])
-def cambiar(id):
-    con = mysql.connection.cursor()
-    con.execute('SELECT * FROM eventos WHERE id = %s', (id,))
-    data = con.fetchall()
-    con.close()
-    return render_template('change.html', data=data)
-
-
-@app.route("/cambiarc/<id>", methods=['POST'])
-def cambiarc(id):
-    if request.method == 'POST':
-        titulo = request.form['titulo']
-        hora = request.form['hora']
-        fecha = request.form['fecha']
-        descripcion = request.form['descripcion']
-        con = mysql.connection.cursor()
-        con.execute("""
-            UPDATE eventos SET titulo = %s, hora = %s, dia = %s, descripcion = %s WHERE id = %s """, (titulo, hora, fecha, descripcion, id,))
-        mysql.connection.commit()
-        return redirect(url_for('index'))
-
-<<<<<<< HEAD
 #configuracion basica y de cookies
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
@@ -357,6 +304,3 @@ if __name__ == '__main__':
 
 
 
-
-=======
->>>>>>> 5a26501645a5688d42dfe8e763de57e8321db4d3
